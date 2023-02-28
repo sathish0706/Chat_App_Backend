61c25bed2893fb85ae44f46b1e8d4e6defb56298
Sathish R <107674282+sathish0706@users.noreply.github.com>
2023-02-28 14:19:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-origin: 'http://localhost:3000',
+origin: 'https://main--roaring-moonbeam-8ed29d.netlify.app',
 credentials: true
 }));
 
